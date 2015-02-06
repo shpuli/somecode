6a1dbe0847d98374dd6b2a5293107c0f4ff55ad5
shpuli <shp.julia@gmail.com>
2015-02-06 22:11:03 +0300

    code examples

---
+++ CatWidthManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Nptv.Presentation.UI;
+
+namespace Nptv.Applications.Test.Entities
+{
+    /// <summary>
+    /// Represents a class with a property that can be set by any method. Used for user's AttachedProperty test.
+    /// </summary>
+    public static class CatWidthManager
+    {
+        /// <summary>
+        /// Registers a dependency property with the specified property name, property type, and owner type.
+        /// </summary>
+        public static readonly DependencyProperty<Single> AttachedWidthProperty =
+         DependencyProperty.RegisterAttached("AttachedWidth", typeof(CatWidthManager), new PropertyMetadata<Single, UIElement>(Single.NaN, AttachedWidthChanged));
+
+        /// <summary>
+        /// Gets the width for the <see cref="UIElement"/>.
+        /// </summary>
+        public static Single GetAttachedWidth(UIElement element)
+        {
+            return element.GetValue(AttachedWidthProperty);
+        }
+
+        /// <summary>
+        /// Sets the width for the <see cref="UIElement"/>.
+        /// </summary>
+        public static void SetAttachedtWidth(UIElement element, Single value)
+        {
+            element.SetValue(AttachedWidthProperty, value);
+        }
+
+        /// <summary>
+        /// The <see cref="Action"/> that will be invoked after each setting of the property.
+        /// </summary>
+        /// <param name="element">The <see cref="UIElement"/> to wich property will be set.</param>
+        /// <param name="args">A value of property.</param>
+        private static void AttachedWidthChanged(UIElement element, DependencyPropertyChangedEventArgs<Single> args)
+        {
+            element.Width = args.NewValueEntry.Value;
+        }
+    }
+}
---
+++ SomeScene.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Some.Applications.Test.Entities;
+using Some.Presentation.Scenes;
+using Some.Presentation.UI;
+using Some.Presentation.UI.Panels;
+
+namespace Some.Applications.Test.Scenes
+{
+    /// <summary>
+    /// Represents scene for testing <see cref="UIElement.DataContext"/> binding.
+    /// </summary>
+    public partial class DataContextTestScene : Scene
+    {
+        /// <summary>
+        /// The instance of the <see cref="ColoredText"/> used for testing data context mappings.
+        /// </summary>
+        private ColoredText _coloredText;
+
+        /// <summary>
+        /// The instance of the <see cref="ResizeableText"/> used for testing data context mappings.
+        /// </summary>
+        private ResizeableText _resizeableText;
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="DataContextTestScene"/> class.
+        /// </summary>
+        public DataContextTestScene()
+        {
+            IsAlternativeTextMeasurementEnabled = true;
+        }
+
+        /// <summary>
+        /// Performs logic when the <see cref="Scene"/> is initialized.
+        /// </summary>
+        /// <remarks>All UI elements created in DataContextScene.sml.</remarks>
+        protected override void OnInitialized()
+        {
+            View.SetValue(TextBlock.FontFamilyProperty, "PF Square Sans Pro");
+
+            ParentBorder.DataContext = new ColoredText("Default Data", Colors.Black);
+            _coloredText = new ColoredText("ColoredText", Colors.Red);
+            _resizeableText = new ResizeableText("ResizeableText", 50, Dock.Bottom, 280f);
+
+            text3.SetBinding(DockPanel.DockProperty, "Dock");
+            text3.SetBinding(CatWidthManager.AttachedWidthProperty, "AttachedWidth");
+
+            btnAddColoredText.Click += (sender, args) => ParentStackPanel.DataContext = _coloredText;
+            btnAddResizeableText.Click += (sender, args) => ChildThirdStackPanel.DataContext = _resizeableText;
+            btnChangeColoredText.Click += (sender, args) => _coloredText.Content += Environment.NewLine + "1st changed";
+            btnChangeResizeableText.Click += (sender, args) => _resizeableText.Content += Environment.NewLine + "2nd changed";
+            btnDeleteAllColoredText.Click += (sender, args) => ParentStackPanel.ClearValue(UIElement.DataContextProperty);
+            btnDeleteResizeableText.Click += (sender, args) => ChildThirdStackPanel.ClearValue(UIElement.DataContextProperty);
+        }
+    }
+}
